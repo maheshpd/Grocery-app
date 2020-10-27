389ef5ba7bd28d81dd85a003e3f116ab2bd64507
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-10-27 12:29:09 +0530

    setup project

---
+++ app/src/main/java/com/createapp/grocery/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.createapp.grocery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+//    anshikarambharti@gmail.com
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //make fullscreen
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+
+        //start login activity after 2sec
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
